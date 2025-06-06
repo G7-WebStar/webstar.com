96302a528bede414fb5ca97a39f34cf5e3d97df0
christianjamss <cj29070@gmail.com>
2025-06-06 15:04:17 +0800

    Added the Sidebar and Main Container Components

---
+++ shared/components/sidebar-for-mobile.php
@@ -0,0 +1,42 @@
+<div class="offcanvas offcanvas-start d-md-none" tabindex="-1" id="sidebarOffcanvas"
+    aria-labelledby="sidebarOffcanvasLabel" style="background-color: rgb(255, 255, 255); width: 250px;">
+    <div class="offcanvas-header">
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <!-- Sidebar content  -->
+        <img src="shared/assets/img/webstar-logo-blue.png" class="img-fluid w-100 py-3 px-2 mb-4" />
+        <ul class="nav flex-column">
+            <li class="nav-item my-1 d-flex align-items-center gap-2 ms-3 my-2">
+                <img src="shared/assets/img/dashboard.png" class="img-fluid" style="width: 30px; height: 30px;">
+                <a class="nav-link text-dark p-0" href="#">Dashboard</a>
+            </li>
+
+            <li class="nav-item my-1 d-flex align-items-center gap-2 ms-3 my-2">
+                <img src="shared/assets/img/courses.png" class="img-fluid" style="width: 30px; height: 30px;">
+                <a class="nav-link text-dark p-0" href="#">Courses</a>
+            </li>
+
+            <li class="nav-item my-1 d-flex align-items-center gap-2 ms-3 my-2">
+                <img src="shared/assets/img/explore.png" class="img-fluid" style="width: 30px; height: 30px;">
+                <a class="nav-link text-dark p-0" href="#">Explore</a>
+            </li>
+
+            <li class="nav-item my-1 d-flex align-items-center gap-2 ms-3 my-2">
+                <img src="shared/assets/img/shop.png" class="img-fluid" style="width: 30px; height: 30px;">
+                <a class="nav-link text-dark p-0" href="#">Shop</a>
+            </li>
+
+            <li class="nav-item my-1 d-flex align-items-center gap-2 ms-3 my-2">
+                <img src="shared/assets/img/games.png" class="img-fluid" style="width: 30px; height: 30px;">
+                <a class="nav-link text-dark p-0" href="#">Mini-games</a>
+            </li>
+
+            <li class="nav-item my-1 d-flex align-items-center gap-2 ms-3 my-2">
+                <img src="shared/assets/img/settings.png" class="img-fluid" style="width: 30px; height: 30px;">
+                <a class="nav-link text-dark p-0" href="#">Settings</a>
+            </li>
+
+        </ul>
+    </div>
+</div>
