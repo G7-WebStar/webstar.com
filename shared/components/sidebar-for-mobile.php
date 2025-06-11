45ec1e6fc960407bcf5bed283d28ea0bbef284fe
christianjamss <cj29070@gmail.com>
2025-06-11 18:30:06 +0800

    Fixed letter spacing

--- shared/components/sidebar-for-mobile.php
+++ shared/components/sidebar-for-mobile.php
@@ -35,7 +35,7 @@
 
             <!-- Profile -->
             <hr>
-            <div class="dropdown mt-auto p-2" style="font-family: var(--Medium);">
+            <div class="dropdown mt-auto p-2" style="font-family: var(--Medium); letter-spacing: -1px;">
                 <a href="#" class="d-flex align-items-center text-decoration-none dropdown-toggle"
                     data-bs-toggle="dropdown" aria-expanded="false">
                     <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
