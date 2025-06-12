bd46488e2d22885b6eed68cb3dca886bc5633d93
Marielle <catomarielle@gmail.com>
2025-06-13 00:13:29 +0800

    Homepage

--- shared/components/navbar-for-mobile.php
+++ shared/components/navbar-for-mobile.php
@@ -2,14 +2,14 @@
     <div class="container-fluid position-relative">
 
         <!-- Toggler -->
-        <button class="navbar-toggler position-absolute start-0" type="button" data-bs-toggle="offcanvas"
+        <button class="navbar-toggler position-absolute start-0 p-1" type="button" data-bs-toggle="offcanvas"
             data-bs-target="#sidebarOffcanvas">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <!-- Logo -->
         <a class="navbar-brand mx-auto" href="#">
-            <img src="shared/assets/img/webstar-logo-blue.png" alt="Webstar" style="height: 40px;">
+            <img src="shared/assets/img/webstar-logo-blue.png" alt="Webstar" style="height: 40px; padding-left: 12px">
         </a>
 
     </div>
