ca200711bcf5fcb4cf1355d53808a91dadc748f1
Marielle <catomarielle@gmail.com>
2025-10-01 00:39:16 +0800

    [FE] and initial [BE] for announcements

--- course-info-contents/files.php
+++ course-info-contents/files.php
@@ -2,12 +2,19 @@
 $fileQuery = "SELECT * FROM files WHERE courseID = '$courseID'";
 $fileResult = executeQuery($fileQuery);
 
-$basePath = "uploads/files/";
 ?>
 
 <?php if (mysqli_num_rows($fileResult) > 0): ?>
     <?php while ($file = mysqli_fetch_assoc($fileResult)): ?>
-        <?php $fileFullPath = $basePath . $file['fileAttachment']; ?>
+        <?php
+        // Decide if link is external or local
+        $filePath = $file['fileLink'];
+
+        // If it’s not starting with http, treat it as local upload
+        if (!preg_match('/^https?:\/\//', $filePath)) {
+            $filePath = $file['fileLink']; // already like uploads/STC Merge.pdf
+        }
+        ?>
 
         <div class="row mb-0 mt-3">
             <div class="col">
@@ -21,7 +28,7 @@ $basePath = "uploads/files/";
                             </div>
                             <div>
                                 <div class="text-sbold text-16 py-1" style="line-height: 1;">
-                                    <?php echo ($file['fileAttachment']); ?>
+                                    <?php echo htmlspecialchars($file['fileAttachment']); ?>
                                 </div>
                                 <div class="text-reg text-12" style="line-height: 1;">
                                     Uploaded <?php echo date("F d, Y", strtotime($file['uploadedAt'])); ?>
@@ -30,10 +37,11 @@ $basePath = "uploads/files/";
                         </div>
 
                         <!-- Download Button -->
-                        <!-- initial download -->
                         <div class="mx-4">
-                            <a href="<?php echo ($fileFullPath); ?>"
-                                download="<?php echo ($file['fileAttachment']); ?>">
+                            <a href="<?php echo $filePath; ?>"
+                                <?php if (!preg_match('/^https?:\/\//', $filePath)) : ?>
+                                download="<?php echo htmlspecialchars($file['fileAttachment']); ?>"
+                                <?php endif; ?>>
                                 <img src="shared/assets/img/dl.png"
                                     alt="Download Icon"
                                     style="width: 16px; height: 20px; cursor:pointer;">
