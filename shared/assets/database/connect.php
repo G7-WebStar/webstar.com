5cf52c09279d3b64471d266539d5d8b06911d19f
christianjamss <cj29070@gmail.com>
2025-11-20 22:25:17 +0800

    Pulled

--- shared/assets/database/connect.php
+++ shared/assets/database/connect.php
@@ -2,7 +2,7 @@
     $dbhost = "localhost";
 	$dbuser = "root";
 	$dbpass = "";
-	$db = "webstar";
+	$db = "webstart";
 
 	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
