4047e29b6461af267a9d2a1b6bbf8be0078e101c
christianjamss <cj29070@gmail.com>
2026-01-10 07:53:43 +0800

    Dom

--- shared/assets/processes/login-process.php
+++ shared/assets/processes/login-process.php
@@ -6,7 +6,7 @@ $login_error = false; // default
 $email_not_found = false;  // Email/username not found
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
-    $conn = new mysqli("localhost", "root", "", "webstar");
+    $conn = new mysqli("localhost", "u977784600_webstar", "WebstarTeamu977784600_", "u977784600_webstar");
    //$conn = new mysqli("localhost", "", "", "");
 
     if ($conn->connect_error) {
